0c7aa4401140e7f6157274e0f8c11f9b810de863
Josue <josuefour@gmail.com>
2021-04-15 12:43:59 -0400

    revert back the typo to original state

--- models/store.py
+++ models/store.py
@@ -2,7 +2,7 @@ from db import db
 
 class StoreModel(db.Model):
     
-    __tablename__ = 'stores'
+    __tablename__ = 'store'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80))
 
