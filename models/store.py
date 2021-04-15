1c09be0e8b53618c2053f5a96d06bb6c79feb811
Josue <josuefour@gmail.com>
2021-04-15 13:25:16 -0400

    fixed typo that was causing an internal error

--- models/store.py
+++ models/store.py
@@ -2,7 +2,7 @@ from db import db
 
 class StoreModel(db.Model):
     
-    __tablename__ = 'store'
+    __tablename__ = 'stores'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80))
 
@@ -12,7 +12,7 @@ class StoreModel(db.Model):
         self.name= name
     
     def json(self):
-        return {'name': self.name, 'items':[ item.json() for item in self.items.all() ]}
+        return {'name': self.name, 'items':[ item.json() for item in self.item.all() ]}
     
     @classmethod
     def find_by_name(cls, name):
